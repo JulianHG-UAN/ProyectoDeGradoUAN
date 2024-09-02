0c64d9b5878b998afe0fb7c0368d87a9bd2b8c94
Julian Hernandez <julhernandez21@uan.edu.co>
2024-09-01 20:37:11 -0500

    Limpieza de vistas

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php require_once "vistas/parte_sup.php"?>
 
 <!-- INICIO contenido principal -->
-<div class="container-fluid">
-    <h1>Contenido principal</h1>    
-</div>
+ 
 <!-- FIN contenido principal -->
 
 <?php require_once "vistas/parte_inf.php"?>
